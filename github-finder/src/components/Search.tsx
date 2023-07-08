922652b16db31511bc1493d25456ce1d1378df3b
eurayssal <eurayssamagalhaes@gmail.com>
2023-07-08 16:22:00 -0300

    Alterações de exibição e de css

--- github-finder/src/components/Search.tsx
+++ github-finder/src/components/Search.tsx
@@ -1,19 +1,31 @@
 type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 import { BsSearch } from "react-icons/bs";
+import classes from './Search.module.css'
 
 const Search = ({ loadUser }: SearchProps) => {
-const [ userName, setUserName] = useState('');
+    const [userName, setUserName] = useState('');
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            loadUser(userName)
+        }
+    }
 
     return (
-        <div>
+        <div className={classes.search}>
             <h2>Busque por um usuário:</h2>
             <p>Conheça os melhores repositórios</p>
-            <div>
-                <input type='text' placeholder='Digite o nome do usuário' onChange={(e) => {setUserName(e.target.value)}}/>
-                <button onClick={() => {loadUser(userName)}}>
+            <div className={classes.search_container}>
+                <input
+                    type='text'
+                    placeholder='Digite o nome do usuário'
+                    onChange={(e) => setUserName(e.target.value)}
+                    onKeyDown={handleKeyDown} />
+
+                <button onClick={() => { loadUser(userName) }}>
                     <BsSearch />
                 </button>
             </div>
--- github-finder/src/routes/Home.tsx
+++ github-finder/src/routes/Home.tsx
@@ -1,20 +1,54 @@
 import { UserProps } from '../types/user';
+import { useState } from 'react';
+
 import Search from '../components/Search';
-import {useState} from 'react';
+import User from '../components/User';
+import Error from '../components/Error';
+import Loader from '../components/Loader';
 
+/*
+TODO
+ -Criar a página de resposta
+ -Css da página de respostas
+-Button de voltar na página de respostas 
+*/
 const Home = () => {
-const [user, setUser] = useState<UserProps | null>(null);
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-const loadUser = async (userName: string) => {
+  const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`)
+    
     const data = await res.json();
 
-    console.log(data)
-}
+    if (res.status === 404){
+      setError(true);
+      return;
+    }
+
+      const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData)
+  }
 
   return (
     <div>
-        <Search loadUser={loadUser}/>
+      <Search loadUser={loadUser} />
+      {user && <User {...user} />}
+      {loading && <Loader></Loader>}
+      {error && <Error></Error>}
     </div>
 
   )
